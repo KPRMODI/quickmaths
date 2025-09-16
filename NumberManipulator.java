08d4754c0605448eac3a9ee76b493f873109d1ce
kpmodi <kpmodi@my.waketech.edu>
2025-09-16 09:36:56 -0400

    finished 2/3 bonus

--- NumberManipulator.java
+++ NumberManipulator.java
@@ -14,27 +14,29 @@ public class NumberManipulator {
         // Store the input in a variable
         Float user_number = getFloatInput("Enter a decimal number: ");
 
-        // Print the smallest integer greater than or equal to the number
+        // Print the smallest integer greater than or equal to the number by ceiling it (raising it up if above 0.5 or more)
         System.out.println("The smallest integer that is greater than or equal to: " + user_number+" is: " + (int) Math.ceil(user_number));
 
-        // Print the largest integer less than or equal to the number
+        // Print the largest integer less than or equal to the number by flooring it (lowering it if lower than 0.4 or les)
         System.out.println("The largest integer that is less than or equal to: " + user_number+" is: " + (int) Math.floor(user_number));
 
-        // Print the integer that is closest to the number (ties go to even)
+        // Print the integer that is closest to the number (ties go to even) by using rint which rounds even numbers lower than 0.5 and rounds odd numbers higher than 0.5 with bakers rounding logic.
         System.out.println("The closest integer to the number with ties going to the even number to: " + user_number+" is: " + Math.rint(user_number));
 
-        // Print the integer closest to the number (standard rounding)
+        // Print the integer closest to the number (standard rounding) nothing too special about this
         System.out.println("The closest integer to the number is: " + user_number+" is: " + (int) Math.round(user_number));
 
         // BONUS:
         // Cast the number to an int and print the character it represents
-        char user_character = (char) Math.round(user_number);
+        char user_character = (char) Math.round(user_number);// casts user_number as a character as long as it meets ASCII standards
         System.out.println(Math.round(user_number) + " --> " + "\'" + user_character + "\'");
         
         // Add 1 to that number and print the next character
-        user_character++;
-        System.out.println(Math.round(user_number+1) + " --> " + "\'" + user_character++ + "\'");
+        user_character++; //increment the variable by 1
+        user_number++; //increment the variable by 1
+        System.out.println(Math.round(user_number) + " --> " + "\'" + user_character + "\'");
 
         // Format a summary table using formatted output
+        // System.out.printf();
     }
 }
